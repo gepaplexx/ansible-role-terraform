615d8396235f06964dea3d6aa1cd60db49fa229d
Clemens Kaserer <ckaserer@gmail.com>
2021-10-27 10:07:59 +0000

    added datacenter to terraform plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,20 @@ provider "tls" {}
 
 # Configure the VMware vSphere Provider
 provider "vsphere" {
+  # user           = "${var.vmware_user}"
+  # password       = "${var.vmware_password}"
+  # vsphere_server = "${var.vmware_host}"
+
   # if you have a self-signed cert
   allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "${var.datacenter}"
 }
 
 resource "vsphere_folder" "folder" {
   path = "TERRAFORM"
   type = "vm"
-  #datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "datacenter" {
+  type = string
+  default = "NTT_Gepaplexx"
+  description = "The vsphere datacenter name used by terrafrom."
+}
